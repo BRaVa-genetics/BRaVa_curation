fd8a8288ee542510f44f9715727924bc91b44655
astheeggeggs <duncan.stuart.palmer@gmail.com>
2025-06-18 13:25:21 -0400

    added sbatch variant meta-analysis script for BRMC

---
+++ meta_analysis/run_meta_analysis_bcftools_metal_variant_gcloud_bmrc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=variant-meta-analysis
+#SBATCH --output=/well/lindgren/dpalmer/logs/%x_%j.log
+#SBATCH --error=/well/lindgren/dpalmer/logs/%x_%j.err
+
+module load BWA/0.7.17-GCCcore-12.3.0
+module load SAMtools/1.18-GCC-12.3.0
+export PATH="$HOME/bin:$PATH"
+export BCFTOOLS_PLUGINS="$HOME/bin"
+
+FILES_VARIANT=$1
+OUT=$2
+
+REGION_FILE="/well/lindgren/dpalmer/protein_coding_regions_hg38_no_padding_no_UTR_v39.bed"
+bcftools +metal -Oz -o $OUT -e 'AF>0.01 & AF<0.99' -R $REGION_FILE $FILES_VARIANT
