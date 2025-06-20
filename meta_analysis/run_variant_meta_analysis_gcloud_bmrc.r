ff7bc4bbbbe47db03544df4e9a8c254e7bac5a8e
astheeggeggs <duncan.stuart.palmer@gmail.com>
2025-06-20 16:15:34 -0400

    fixed edge case when variant input file to meta-analysis is empty

--- meta_analysis/run_variant_meta_analysis_gcloud_bmrc.r
+++ meta_analysis/run_variant_meta_analysis_gcloud_bmrc.r
@@ -74,10 +74,12 @@ main <- function(args)
 			files_vcf <- (results_dt %>% 
 				filter(phenotypeID == phe, sex == s))$filename
 			# For each vcf file - ensure that it is not empty.
-			empty_vcfs <- which(
-				sapply(files_vcf, function(f) file.info(f)$size == 0))
-			if (length(empty_vcfs) > 0) {
-				files_vcf <- files_vcf[-empty_vcfs]
+			if (length(files_vcf) > 0) {
+				empty_vcfs <- which(
+					sapply(files_vcf, function(f) file.info(f)$size == 0))
+				if (length(empty_vcfs) > 0) {
+					files_vcf <- files_vcf[-empty_vcfs]
+				}
 			}
 			if (length(files_vcf) <= 1) { 
 				cat(paste("Either the phenotype is not present, or there is",
@@ -122,11 +124,14 @@ main <- function(args)
 			for (p in c("AFR", "AMR", "EAS", "EUR", "SAS")) {
 				files_vcf_tmp <- (to_subset %>% filter(pop == !!p))$filename
 				# For each vcf file - ensure that it is not empty.
-				empty_vcfs <- which(
-					sapply(files_vcf_tmp, function(f) file.info(f)$size == 0))
-				if (length(empty_vcfs) > 0) {
-					files_vcf_tmp <- files_vcf_tmp[-empty_vcfs]
+				if (length(files_vcf_tmp) > 0) {
+					empty_vcfs <- which(
+						sapply(files_vcf_tmp, function(f) file.info(f)$size == 0))
+					if (length(empty_vcfs) > 0) {
+						files_vcf_tmp <- files_vcf_tmp[-empty_vcfs]
+					}
 				}
+				
 				if (length(files_vcf_tmp) <= 1) { 
 					cat("Either the phenotype is not present, or there is",
 						"only a single file for:\n")
@@ -170,10 +175,12 @@ main <- function(args)
 			
 			files_vcf_tmp <- (to_subset %>% filter(pop != "EUR"))$filename
 			# For each vcf file - ensure that it is not empty.
-			empty_vcfs <- which(
-				sapply(files_vcf_tmp, function(f) file.info(f)$size == 0))
-			if (length(empty_vcfs) > 0) {
-				files_vcf_tmp <- files_vcf_tmp[-empty_vcfs]
+			if (length(files_vcf_tmp) > 0) {
+				empty_vcfs <- which(
+					sapply(files_vcf_tmp, function(f) file.info(f)$size == 0))
+				if (length(empty_vcfs) > 0) {
+					files_vcf_tmp <- files_vcf_tmp[-empty_vcfs]
+				}
 			}
 				
 			if (length(files_vcf_tmp) <= 1) { 
@@ -213,10 +220,12 @@ main <- function(args)
 				files_vcf <- (results_dt_tmp %>% 
 					filter(phenotypeID == phe, sex == s))$filename
 				# For each vcf file - ensure that it is not empty.
-				empty_vcfs <- which(
-					sapply(files_vcf, function(f) file.info(f)$size == 0))
-				if (length(empty_vcfs) > 0) {
-					files_vcf <- files_vcf[-empty_vcfs]
+				if (length(files_vcf) > 0) {
+					empty_vcfs <- which(
+						sapply(files_vcf, function(f) file.info(f)$size == 0))
+					if (length(empty_vcfs) > 0) {
+						files_vcf <- files_vcf[-empty_vcfs]
+					}
 				}
 				
 				if (length(files_vcf) <= 1) { 
@@ -257,10 +266,12 @@ main <- function(args)
 			files_vcf <- (results_dt %>% 
 				filter(phenotypeID == phe, sex == s))$filename
 			# For each vcf file - ensure that it is not empty.
-			empty_vcfs <- which(
-				sapply(files_vcf, function(f) file.info(f)$size == 0))
-			if (length(empty_vcfs) > 0) {
+			if (length(files_vcf) > 0) {
+				empty_vcfs <- which(
+					sapply(files_vcf, function(f) file.info(f)$size == 0))
+				if (length(empty_vcfs) > 0) {
 					files_vcf <- files_vcf[-empty_vcfs]
+				}
 			}
 			
 			if (length(files_vcf) <= 1) { 
