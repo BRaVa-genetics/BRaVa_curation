5f82c707cb210555ffe0d1f3ecf96b8963379bee
astheeggeggs <duncan.stuart.palmer@gmail.com>
2025-10-11 17:27:41 +0100

    added gene locations for variant plotting

--- meta_analysis/manuscript_figures/variant_output_results_for_plotting.r
+++ meta_analysis/manuscript_figures/variant_output_results_for_plotting.r
@@ -113,4 +113,4 @@ meta <- rbindlist(meta_list)
 meta$ancestry <- "all"
 
 fwrite(meta, sep = "\t", quote=FALSE,
-	file="/well/lindgren/dpalmer/BRaVa_meta-analysis_outputs/all_assocs_from_meta_060625.tsv.gz")
+	file="/well/lindgren/dpalmer/BRaVa_meta-analysis_outputs/all_variant_assocs_from_meta_101025.tsv.gz")
