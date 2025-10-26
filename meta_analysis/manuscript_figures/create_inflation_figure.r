3324ad305d68d07bc4da9d721ab2cd039c25706b
astheeggeggs <duncan.stuart.palmer@gmail.com>
2025-10-26 09:26:17 +0000

    changed axis size

--- meta_analysis/manuscript_figures/create_inflation_figure.r
+++ meta_analysis/manuscript_figures/create_inflation_figure.r
@@ -72,7 +72,7 @@ for (anc in unique(dt$ancestry)) {
     ) +
     theme(
       plot.title = element_text(size = 16, face = "bold", hjust = 0),
-      axis.text.x = element_text(angle = 45, hjust = 1),
+      axis.text.x = element_text(angle = 45, hjust = 1, size=7),
       strip.text = element_text(size = 10),
       legend.position = "none")
   plots[[anc]] <- p
